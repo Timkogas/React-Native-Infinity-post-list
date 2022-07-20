759f174c2048f9e461cefea7762789bd5fbe6c9c
Timur Ermolaev <timkogas@gmail.com>
2022-07-20 14:47:49 +0300

    Реализовал flatlist

--- src/components/PostsWrapper/PostsWrapper.js
+++ src/components/PostsWrapper/PostsWrapper.js
@@ -3,22 +3,20 @@ import Post from "./Post/Post";
 
 export default function PostsWrapper({posts}) {
   return (
-    <View style={styles.postsWrapper}>
-      {posts.map((post)=>{
-        return (
-          <Post
-            key={post.data.id}
-            img={post.data.thumbnail}
-            text={post.data.title}
-          />
-        )
-      })}
-    </View>
+
+    <FlatList 
+      data={posts}
+      style={{width: "100%"}}
+      renderItem={({item}) => 
+      <Post 
+        keyExtractor={(item)=>{item.data.id}}
+        img={item.data.thumbnail ? item.data.url_overridden_by_dest : item.data.thumbnail}
+        text={item.data.title}
+      />
+    
+    }
+      keyExtractor={post => post.id}
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  postsWrapper: {
-  },
-})
 
